12d38128f8b1ea4bbd474a163d425316f10ac85f
Alexandre Zin <alexandre.zin.silva@gmail.com>
2020-05-06 16:44:53 -0300

    Conneting to DB

---
+++ src/db/DB.java
@@ -0,0 +1,49 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+	private static Connection connection = null;
+	
+	public static Connection openConnection() {
+		try {
+			if (connection == null) {
+				Properties properties = loadProperties();
+				String url = properties.getProperty("dburl");
+				connection = DriverManager.getConnection(url, properties);
+			}
+		}
+		catch(SQLException exception) {
+			throw new DbException(exception.getMessage());
+		}
+		return connection;
+	}
+	
+	public static void closeConnection() {
+		try {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+		catch (SQLException exception) {
+			throw new DbException(exception.getMessage());
+		}
+	}
+	
+	private static Properties loadProperties() {
+		try(FileInputStream fs = new FileInputStream("db.properties")){
+			Properties properties = new Properties();
+			properties.load(fs);
+			return properties;
+		}
+		catch (IOException exception) {
+			throw new DbException(exception.getMessage());
+		}
+	}
+}
