6d0907599e4eade2c841efe9c54a1e2f23a191da
Alexandre Zin <alexandre.zin.silva@gmail.com>
2020-05-07 09:39:58 -0300

    Getting data from Db

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,36 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection conn = DB.openConnection();
-		DB.closeConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
 		
-
+		try {
+			conn = DB.openConnection();
+			st = conn.createStatement();
+			rs = st.executeQuery("select * from department");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}
+		catch (SQLException exception) {
+			System.out.println(exception.getMessage());
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 	}
 
 }
